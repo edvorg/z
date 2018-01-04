820a7c8195c3107cbd092448da90b652cc0af65e
Edward Knyshov <edvorg@gmail.com>
2018-01-04 12:54:53 +0700

    target java 7

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/edvorg/z"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :javac-options ["-target" "1.7" "-source" "1.7"])
