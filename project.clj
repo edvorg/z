68e66c7b7a99b81ad62ecabc76c7837a586477f4
Edward Knyshov <edvorg@gmail.com>
2018-01-07 12:28:43 +0700

    java 7

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :javac-options ["-target" "1.7" "-source" "1.7"])
+  :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"])
